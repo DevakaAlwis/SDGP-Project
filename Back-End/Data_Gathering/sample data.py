66f27019521b7bbf4f61ec3f0160a7d30577502a
Devaka Alwis <devakaalwis2001@gmail.com>
2023-02-15 21:29:16 +0530

    modify the sample data py

--- Back-End/Data_Gathering/sample data.py
+++ Back-End/Data_Gathering/sample data.py
@@ -1,19 +1,62 @@
 import json
 
-def convertToDictionary(temp_data):
-    if(len(temp_data)==1):
-        temp_data.update(temp_data)
-    elif(len(temp_data)!=0):
-        print(len(temp_data))
-        # for i in temp_data:
-        #     convertToDictionary(temp_data)
+# def convertNestedToDictionary(temp_data,data):
+#     if(len(temp_data)==1):  #if only 1 item 
+#         temp_data.update(temp_data)
+#     elif(len(temp_data)>1):    #multi for loop print one by one
+#         print(len(temp_data))
+#         # for i in temp_data:
+#         #     convertToDictionary(temp_data)
 
 
-json_data ='{"offer": {"currency": "USD","price": 499.99},"request_parameters": {"epid": "325389825157","ebay_domain": "ebay.com"},"seller": {"name": "orkmastermcgee"},"product": {"rating": 4.8,"images": [{"link": "https://i.ebayimg.com/images/g/3G8AAOSwNslhoSZW/s-l1600.jpg"},{"link": "https://i.ebayimg.com/images/g/4~MAAOSwC7Vh4JaJ/s-l1600.jpg"}],"review_count": 3214,"title": "PS5 Sony PlayStation 5 Console Disc Version!"}}'
+json_data ='{"a":12,"offer": {"currency": "USD","price": 499.99},"request_parameters": {"epid": "325389825157","ebay_domain": "ebay.com"},"seller": {"name": "orkmastermcgee"},"product": {"rating": 4.8,"images": [{"link": "https://i.ebayimg.com/images/g/3G8AAOSwNslhoSZW/s-l1600.jpg"},{"link": "https://i.ebayimg.com/images/g/4~MAAOSwC7Vh4JaJ/s-l1600.jpg"}],"review_count": 3214,"title": "PS5 Sony PlayStation 5 Console Disc Version!"}}'
 
 #parsed_json
 parsed_json = json.loads(json_data)     #convert the json data string to dictionary 
-convertToDictionary(parsed_json)
+
+data={}
+
+# if(len(i)==1):
+#         print("Hiiiiiiiii")
+#         # print(parsed_json.get(i))
+#     elif(len(i)>1):
+#         if(len(i)==1):  #if only 1 item 
+#             data.update(i)  #data is the new dict
+#         elif(len(i)>1):    #multi for loop print one by one
+
+def s(i,data):
+    for j in i:
+        print(i.get(j))
+        # if(len(j)==1):
+        #     print(parsed_json.get(j))
+        # elif(len(j)>1):
+        #     s(j,data)
+
+
+# s(parsed_json,data)
+
+for i in parsed_json:
+    if(len(i)==1):
+        print(parsed_json.get(i))
+    elif(len(i)>1):
+        s(parsed_json.get(i),data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #nested dict remove the nested
     #multi nested dict
@@ -25,10 +68,6 @@ convertToDictionary(parsed_json)
 #insert to the db
 
 
-data={}
-if(data.get("epid") or data.get("id")):
-    if(data.get("epid")):
-        data={}
 
 
 
@@ -47,7 +86,6 @@ if(data.get("epid") or data.get("id")):
 # print(type(parsed_json))
 
 
-# if()
 
 # epid = parsed_json["request_parameters"]["epid"]
 # ebay_domain = parsed_json["request_parameters"]["ebay_domain"]
