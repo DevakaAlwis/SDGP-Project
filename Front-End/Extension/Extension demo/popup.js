e7ebeeae5a1e661dd0eb75899d53006168371ab3
Dimuthu Jayathunga <dimuthuchanaka06@gmail.com>
2023-03-22 03:18:52 +0530

    Update
    Update popup.css, popup.html & popup.js (create Product URL clickable)

--- Front-End/Extension/Extension demo/popup.js
+++ Front-End/Extension/Extension demo/popup.js
@@ -20,9 +20,7 @@ chrome.storage.local.get(
       // const productSellerNameEl = document.getElementById("product-seller");
       const productSiteNameEl = document.getElementById("product-site");
       const productRatingEl = document.getElementById("product-rating");
-      const productReviewCountEl = document.getElementById(
-        "product-numberOfReviews"
-      );
+      const productReviewCountEl = document.getElementById("product-numberOfReviews");
       const productURLEl = document.getElementById("product-url");
   
       imageURL = result.productImage;
@@ -34,7 +32,8 @@ chrome.storage.local.get(
       productSiteNameEl.innerText = result.productSiteName;
       productRatingEl.innerText = result.productRating;
       productReviewCountEl.innerText = result.productReviewCount;
-      productURLEl.innerText = result.productURL;
+      productURLEl.href = result.productURL;
+      productURLEl.textContent = result.productURL;
     }
   );
   
