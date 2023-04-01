4f98ad3fb58d6866f6c9ef0b402b393765881514
Avishbimsara <94750659+avibimsara@users.noreply.github.com>
2023-04-02 00:43:32 +0530

    Updating python files and adding the corect sentikment model training notebook

--- Back-End/ML/product_Update_db.py
+++ Back-End/ML/product_Update_db.py
@@ -4,17 +4,17 @@ from database_connection import db, reviews_collection, product_collection
 
 
 def updateSentimentLabels():
- 
+
     try:
-        #reviews Collection
+        # reviews Collection
         reviews = reviews_collection.find()
 
-        #product Collection
+        # product Collection
         products = product_collection.find()
     except CollectionInvalid as e:
         print(f'Collection not available {e}')
-    
-    #update the values
+
+    # update the values
     for product in products:
         productID = product["productId"]
         item_id = product['_id']
@@ -24,39 +24,41 @@ def updateSentimentLabels():
         foundReviewCount = 0
         foundReviewsRating = 0
         for review in reviews:
-            foundReviewCount+=1     #calculate found review count
-            reviewRating = review["reviewRating"] 
-            foundReviewsRating+=float(review["reviewRating"])    #calculate found review rating
+            foundReviewCount += 1  # calculate found review count
+            reviewRating = review["reviewRating"]
+            # calculate found review rating
+            foundReviewsRating += float(review["reviewRating"])
             sentimentLabel = review["sentimentLabel"]
-            if(sentimentLabel == "positive"):
-                positive+=1
-            elif(sentimentLabel == "neutral"):
-                neutral+=1
-            elif(sentimentLabel == "negative"):
-                negative+=1
-        averageReviewRating=0
+            if (sentimentLabel == "positive"):
+                positive += 1
+            elif (sentimentLabel == "neutral"):
+                neutral += 1
+            elif (sentimentLabel == "negative"):
+                negative += 1
+        averageReviewRating = 0
 
-        #zore division error validation
-        if(foundReviewCount!=0):    
-            averageReviewRating = round(foundReviewsRating/foundReviewCount,1)   #calculate the average of the review rating
-    
+        # zore division error validation
+        if (foundReviewCount != 0):
+            # calculate the average of the review rating
+            averageReviewRating = round(foundReviewsRating/foundReviewCount, 1)
+
         try:
-            product_collection.update_one({"_id":item_id}, {'$set': {'positiveReviews': positive}})  
-            product_collection.update_one({"_id":item_id}, {'$set': {'neutralReviews': neutral}})  
-            product_collection.update_one({"_id":item_id}, {'$set': {'negativeReviews': negative}})  
-            product_collection.update_one({"_id":item_id}, {'$set': {'foundReviewCount': foundReviewCount}})
-            product_collection.update_one({"_id":item_id}, {'$set': {'foundReviewRating': averageReviewRating}})
-            print("foundReviewCount: ",foundReviewCount," | positive: ",positive," | neutral: ",neutral," | negative: ",negative," | averageReviewRating: ",averageReviewRating)
+            product_collection.update_one(
+                {"_id": item_id}, {'$set': {'positiveReviews': positive}})
+            product_collection.update_one(
+                {"_id": item_id}, {'$set': {'neutralReviews': neutral}})
+            product_collection.update_one(
+                {"_id": item_id}, {'$set': {'negativeReviews': negative}})
+            product_collection.update_one(
+                {"_id": item_id}, {'$set': {'foundReviewCount': foundReviewCount}})
+            product_collection.update_one(
+                {"_id": item_id}, {'$set': {'foundReviewRating': averageReviewRating}})
+            print("foundReviewCount: ", foundReviewCount, " | positive: ", positive, " | neutral: ",
+                  neutral, " | negative: ", negative, " | averageReviewRating: ", averageReviewRating)
         except WriteError as e:
-            print(f' An error ocured while updating the database {e}');
+            print(f' An error ocured while updating the database {e}')
         except OperationFailure as e:
-            print(f'An error occured while reading data from collection {e}')    
+            print(f'An error occured while reading data from collection {e}')
 
-            
+
 updateSentimentLabels()
-
-
-
-
-
-
