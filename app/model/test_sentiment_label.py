b790fe268cb722cc4f2b07b991dce2b6ea202d9c
Avishbimsara <94750659+avibimsara@users.noreply.github.com>
2023-04-05 12:22:55 +0530

    adding test cases for sentiment label and trustscore adding to database

---
+++ app/model/test_sentiment_label.py
@@ -0,0 +1,44 @@
+from sentiment_label import runSentimentLabelModel
+from pymongo.errors import WriteError
+from unittest.mock import patch, Mock
+import unittest
+
+
+class TestrunSentimentLabelModel(unittest.TestCase):
+
+    def setUp(self):
+        self.reviews_collection = Mock()
+
+    # test function that test wether correct message is given when colletion is empty
+
+    def test_collection_empty(self):
+        self.reviews_collection.count_documents.return_value = 0
+        output = runSentimentLabelModel(self.reviews_collection)
+        self.assertEqual(output, 'No reviews found in the collection')
+
+    # test function to test model load failure
+
+    def test_model_load_fail(self):
+        with patch('joblib.load', side_effect=FileNotFoundError('Model not found')):
+            # calling the function that is tested
+            output = runSentimentLabelModel(
+                self.reviews_collection)
+            # asserting the function returns ecpected result
+            self.assertEqual(output, 'Unable to load model.')
+
+    # test function that test if correct messege is given when product update is empty
+
+    # def test_update_field_failiure(self):
+    #     self.reviews_collection.count_documents.return_value = 1
+    #     self.reviews_collection.find.return_value = [
+    #         {'_id': '1', 'reviewText': 'This is a great product.'}]
+    #     self.reviews_collection.update_one.side_effect = WriteError(
+    #         'Update failed')
+    #     result = runSentimentLabelModel(self.reviews_collection)
+    #     self.assertEqual(
+    #         result, ' An error ocured while updating the database, 0 errors.'
+    #     )
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ app/model/test_tustworth_score.py
@@ -0,0 +1,66 @@
+import unittest
+from trustworth_score import runTrustWorthyScoreModel
+from pymongo.errors import WriteError
+from unittest.mock import patch, Mock
+import numpy as np
+import joblib
+
+
+class TestrunTrustWorthyScoreModel(unittest.TestCase):
+
+    def setUp(self):
+        # create a mock product
+        self.product = {
+            '_id': '123',
+            'foundReviewRating': 4.5,
+            'foundReviewCount': 10,
+            'positiveReviews': 8,
+            'neutralReviews': 1,
+            'negativeReviews': 1
+        }
+
+        # creating a mock product collection
+        self.product_collection = Mock()
+        self.product_collection.find.return_value = [self.product]
+
+        # creating a mock loaded model
+        self.loaded_rf_model = Mock()
+        self.loaded_rf_model.predict.return_value = np.array([90])
+
+    # function to test model load failure
+    def test_model_load_fail(self):
+        with patch('joblib.load', side_effect=FileNotFoundError('Model not found')):
+            # calling the function that is tested
+            output = runTrustWorthyScoreModel(self.product_collection)
+            # asserting the function returns ecpected result
+            self.assertEqual(output, 'Unable to load model.')
+
+    def test_trustscore_values(self):
+        products = self.product_collection.find()
+        loaded_rf_model = joblib.load('D:/sentiment_model/rf_model.sav')
+        for product in products:
+            item_id = product['_id']
+            try:
+                reviewCount = product['foundReviewCount']
+            except KeyError as e:
+                self.fail(f'No review counts found. {e}')
+                break
+            trustworth_score = 0
+            # if the review count is 0 trustworthscore is 0
+            if (reviewCount != 0):
+                features = [[product['foundReviewRating'], product['foundReviewCount'],
+                             product['positiveReviews'], product['neutralReviews'], product['negativeReviews']]]
+                trustworth_score = loaded_rf_model.predict(features)
+                trustworth_score = round(float(trustworth_score[0]), 1)
+            self.assertGreaterEqual(trustworth_score, 0)
+            self.assertLessEqual(trustworth_score, 100)
+            try:
+                self.product_collection.update_one(
+                    {"_id": item_id}, {'$set': {'TrustworthyScore': trustworth_score}})  # u
+            except WriteError as e:
+                self.fail(
+                    f'An error occured while reading data from collection {e}')
+
+
+if __name__ == '__main__':
+    unittest.main()
